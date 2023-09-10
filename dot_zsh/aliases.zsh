505827c5221b4724b3a6d0a38b8a40481027a888
Elvis Diniz <elvisdiniz@outlook.com>
2023-09-10 15:31:42 -0300

    create a new alias

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -2,5 +2,11 @@
 [[ ! -x "$(command -v bat)" ]] || alias cat="bat --style=auto"
 [[ ! -x "$(command -v batcat)" ]] || alias cat="batcat --style=auto"
 [[ ! -x "$(command -v mktemp)" ]] || alias cdtemp="cd $(mktemp -d)"
+
+## wsl specific configs
+# create a alias to notepad++:
 [[ ! -x "$(command -v notepad++.exe)" ]] || alias npp="notepad++.exe"
+# create a alias to ssh.exe:
 [[ ! -x "$(command -v ssh.exe)" ]] || alias ssh="ssh.exe"
+# create a alias to git.exe:
+[[ ! -x "$(command -v git.exe)" ]] || alias git="git.exe"
