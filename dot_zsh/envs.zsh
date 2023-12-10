33560caa0f069580c53332b0311a8eb7c1c032d3
Elvis Diniz <elvisdiniz@outlook.com>
2023-12-09 23:17:54 -0300

    load acme.sh env

--- dot_zsh/envs.zsh
+++ dot_zsh/envs.zsh
@@ -1,2 +1,4 @@
 [[ ! -x "$(command -v vim)" ]] || export EDITOR=vim
 [[ -x "$(command -v vim)" || ! -x "$(command -v vi)" ]] || export EDITOR=vi
+
+[[ ! -f ~/.acme.sh/acme.sh.env ]] || ~/.acme.sh/acme.sh.env
