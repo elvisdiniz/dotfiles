02f38537bd68368c8a3c4fa8baeda4f1e5b7ba4c
Elvis Diniz <elvisdiniz@outlook.com>
2024-06-27 12:52:22 +0000

    create DEFAULT_USER env var

--- dot_zsh/envs.zsh
+++ dot_zsh/envs.zsh
@@ -2,3 +2,7 @@
 [[ -x "$(command -v vim)" || ! -x "$(command -v vi)" ]] || export EDITOR=vi
 
 [[ ! -f ~/.acme.sh/acme.sh.env ]] || source ~/.acme.sh/acme.sh.env
+
+# Default username
+export DEFAULT_USER=elvisdiniz
+
