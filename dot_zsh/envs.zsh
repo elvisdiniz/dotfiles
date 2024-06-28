937594637c2d021096db4b90212ca2ac418c87ba
Elvis Diniz <elvisdiniz@outlook.com>
2024-06-28 17:50:58 +0000

    set nvim as default editor

--- dot_zsh/envs.zsh
+++ dot_zsh/envs.zsh
@@ -1,4 +1,5 @@
 [[ ! -x "$(command -v vim)" ]] || export EDITOR=vim
+[[ ! -x "$(command -v nvim)" ]] || export EDITOR=nvim
 [[ -x "$(command -v vim)" || ! -x "$(command -v vi)" ]] || export EDITOR=vi
 
 [[ ! -f ~/.acme.sh/acme.sh.env ]] || source ~/.acme.sh/acme.sh.env
