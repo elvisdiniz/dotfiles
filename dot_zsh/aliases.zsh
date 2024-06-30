342a080e4212ba7c96836848ef342641e5cb6f7a
Elvis Diniz <elvisdiniz@outlook.com>
2024-06-29 23:14:36 -0300

    update aliases

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -1,14 +1,12 @@
 [[ ! -x "$(command -v exa)" ]] || alias ls="exa --icons --group-directories-first"
 [[ ! -x "$(command -v dust)" ]] || alias du="dust"
-#[[ ! -x "$(command -v bat)" ]] || alias cat="bat --style=auto"
-[[ ! -x "$(command -v batcat)" ]] || alias cat="batcat --style=auto"
+[[ ! -x "$(command -v bat)" ]] || alias bat="bat --style=auto"
+[[ ! -x "$(command -v batcat)" ]] || alias bat="batcat --style=auto"
 [[ ! -x "$(command -v mktemp)" ]] || alias cdtemp="cd $(mktemp -d)"
 [[ -x "$(command -v vim)" || ! -x "$(command -v vi)" ]] || alias vim="vi"
 [[ -x "$(command -v open)" || ! -x "$(command -v xdg-open)" ]] || alias open="xdg-open"
 [[ ! -x "$(command -v chezmoi)" ]] || alias chupg="chezmoi upgrade"; alias chupd="chezmoi update"
 [[ ! -x "$(command -v btm)" ]] || alias btm="btm --mem_as_value -m"
-
-#alias ccat="/bin/cat"
 
 ## wsl specific configs
 # create a alias to notepad++:
