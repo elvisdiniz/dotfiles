0f4470dff5ea90853b2f9364b312cc8ad11c4e8e
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-01 20:02:59 +0000

    only use fd if is installed

--- dot_zsh/plugins/zsh-fzf/fzf-init.zsh
+++ dot_zsh/plugins/zsh-fzf/fzf-init.zsh
@@ -1,6 +1,5 @@
 [[ -x "$(command -v fzf)" ]] || exit 0
 [[ -x "$(command -v eza)" ]] || exit 0
-[[ -x "$(command -v fd)" ]] || exit 0
 # ---- FZF -----
 
 # Set up fzf key bindings and fuzzy completion
@@ -17,22 +16,24 @@ cyan="#2CF9ED"
 export FZF_DEFAULT_OPTS="--color=fg:${fg},bg:${bg},hl:${purple},fg+:${fg},bg+:${bg_highlight},hl+:${purple},info:${blue},prompt:${cyan},pointer:${cyan},marker:${cyan},spinner:${cyan},header:${cyan}"
 
 # -- Use fd instead of fzf --
+if [[ -x "$(command -v fd)" ]]; then
 
-export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+  export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
 
-# Use fd (https://github.com/sharkdp/fd) for listing path candidates.
-# - The first argument to the function ($1) is the base path to start traversal
-# - See the source code (completion.{bash,zsh}) for the details.
-_fzf_compgen_path() {
-  fd --hidden --exclude .git . "$1"
-}
+  # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
+  # - The first argument to the function ($1) is the base path to start traversal
+  # - See the source code (completion.{bash,zsh}) for the details.
+  _fzf_compgen_path() {
+    fd --hidden --exclude .git . "$1"
+  }
 
-# Use fd to generate the list for directory completion
-_fzf_compgen_dir() {
-  fd --type=d --hidden --exclude .git . "$1"
-}
+  # Use fd to generate the list for directory completion
+  _fzf_compgen_dir() {
+    fd --type=d --hidden --exclude .git . "$1"
+  }
+fi
 
 source ~/.zsh/plugins/zsh-fzf/fzf-git.sh
 
