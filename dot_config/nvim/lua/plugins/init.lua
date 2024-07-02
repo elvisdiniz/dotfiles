4c05975cb1ad94fd6ef101212695df31067610c4
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-02 19:59:56 -0300

    install nvim-tmux-navigator plugin

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -7,6 +7,16 @@ return {
     end,
   },
 
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      require('nvim-tmux-navigation').setup({})
+      vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
+      vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
+      vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
+      vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
+    end,
+  }
   -- These are some examples, uncomment them if you want to see them work!
   -- {
   --   "neovim/nvim-lspconfig",
