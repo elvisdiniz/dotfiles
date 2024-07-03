ac5e92aaaefa305961316fd0e9a4c3780aaac13a
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-03 20:23:15 +0000

    if in tmux, open on fzf on new pane

--- dot_zsh/plugins.zsh.d/fzf/fzf.init.zsh
+++ dot_zsh/plugins.zsh.d/fzf/fzf.init.zsh
@@ -5,7 +5,8 @@ source <(~/.local/bin/fzf --zsh)
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--height 40% --tmux bottom,40% --layout reverse --border top"
 
 # -- Use fd instead of fzf --
 if [[ -x "$(command -v fd)" ]]; then
