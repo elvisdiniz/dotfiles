ce41cb44acc15f136013ed1d14b1de8a17fb73ed
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-03 19:38:02 +0000

    update plugins filenames

--- dot_zsh/plugins.zsh
+++ dot_zsh/plugins.zsh
@@ -1,5 +1,5 @@
 # init plugins in ~/.zsh/custom/plugins/zsh-* directory
-for file in ~/.zsh/plugins.zsh.d/**/*-init.zsh; do
+for file in $(dirname $0)/plugins.zsh.d/**/*.init.zsh; do
     source "$file"
 done
 
