fadba4cd10648c96ad21ea13fe8c57a8c22484f6
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-02 22:35:01 -0300

    fix bug on ssh connection identification

--- dot_zsh/lib/is-ssh.sh
+++ dot_zsh/lib/is-ssh.sh
@@ -3,7 +3,7 @@
 function is_ssh() {
   p=${1:-$PPID}
   [ "$p" -eq 0 ]     && { echo no; return 1; }
-  read pid ppid name < <(ps -o pid= -o ppid= -o comm= -p $p) 
+  read pid ppid name < <(ps ax -o pid= -o ppid= -o comm= | /bin/grep -E -i "^\s*$p\s")
   [[ "$name" =~ sshd ]] && { echo yes; return 0; }
   [ "$ppid" -le 1 ]     && { echo no; return 1; }
   is_ssh $ppid
