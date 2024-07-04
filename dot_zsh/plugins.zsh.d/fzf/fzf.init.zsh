caece41377ba06ca96680aefa871063f702382af
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-04 00:21:26 -0300

    reformat file

--- dot_zsh/plugins.zsh.d/fzf/fzf.init.zsh
+++ dot_zsh/plugins.zsh.d/fzf/fzf.init.zsh
@@ -43,9 +43,17 @@ _fzf_comprun() {
   shift
 
   case "$command" in
-    cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
-    export|unset) fzf --preview "eval 'echo \${}'"         "$@" ;;
-    ssh)          fzf --preview 'kdig {}'                   "$@" ;;
-    *)            fzf --preview "$show_file_or_dir_preview" "$@" ;;
+  cd)
+    fzf --preview 'eza --tree --color=always {} | head -200' "$@"
+    ;;
+  export | unset)
+    fzf --preview "eval 'echo \${}'" "$@"
+    ;;
+  ssh)
+    fzf --preview 'kdig {}' "$@"
+    ;;
+  *)
+    fzf --preview "$show_file_or_dir_preview" "$@"
+    ;;
   esac
 }
