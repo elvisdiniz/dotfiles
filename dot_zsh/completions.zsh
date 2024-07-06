cd48239d49faec474d9600a06c1d16f307c3d603
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-06 13:34:44 +0000

    remove docker zsh plugin

--- dot_zsh/completions.zsh
+++ dot_zsh/completions.zsh
@@ -2,7 +2,3 @@ for file in ~/.zsh/completions.zsh.d/**/*.completion.zsh; do
     source "$file"
 done
 
-# load completions from cache dir
-for file in $HOME/.cache/zinit/completions/_*; do
-    eval "$(/bin/cat $file)"
-done
