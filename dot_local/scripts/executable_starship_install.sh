3e3fd7b6014b10a6ad97f52bb5a2223002284b12
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-08 20:20:17 -0300

    put starship install in a script

---
+++ dot_local/scripts/executable_starship_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# check if starship is available on /usr/bin. If not, install on $BIN_HOME
+if [ -x "/usr/bin/starship" ] || [ -x "/usr/local/bin/starship" ]; then
+    [ -x "$BIN_HOME/starship" ] && rm "$BIN_HOME/starship"
+else
+    [ -x "$BIN_HOME/starship" ] || curl -sS https://starship.rs/install.sh | sh -s -- -b $BIN_HOME -y
+fi
