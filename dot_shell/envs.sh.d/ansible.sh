575591edfbbf58ffa8d27e4d2e514e1cf33af0c6
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-09 03:48:57 +0000

    setup ansible to use xdg dirs

---
+++ dot_shell/envs.sh.d/ansible.sh
@@ -0,0 +1,3 @@
+export ANSIBLE_HOME="${XDG_CONFIG_HOME:-$HOME/.config}/ansible"
+export ANSIBLE_CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/ansible/ansible.cfg"
+export ANSIBLE_GALAXY_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/ansible/galaxy_cache"
