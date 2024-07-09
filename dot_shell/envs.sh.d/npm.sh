e56d51fbd39e9fe006ea8b61a192dce5f90ebc8a
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-09 02:54:35 +0000

    setup npm to use xdg home dirs

---
+++ dot_shell/envs.sh.d/npm.sh
@@ -0,0 +1,6 @@
+export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/npm/npmrc"
+export NPM_PREFIX_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/npm"
+export NPM_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}/npm"
+export NPM_INIT_MODULE="${XDG_CONFIG_HOME:-$HOME/.config}/npm/config/npm-init.js"
+export NPM_LOGS_DIR="${XDG_STATE_HOME:-$HOME/.local/state}/npm/logs"
+export NVM_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/nvm"
