25a4962bdafc7175297b882f2b07cf619cff8226
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-11 23:24:13 +0000

    load asdf completions on envs file

--- dot_config/exact_shell/exact_envs.sh.d/asdf.sh
+++ dot_config/exact_shell/exact_envs.sh.d/asdf.sh
@@ -3,4 +3,5 @@ export ASDF_DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/asdf"
 if [ -f "$ASDF_DATA_DIR/asdf.sh" ]; then
     source "$ASDF_DATA_DIR/asdf.sh"
     export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME:-$HOME/.config}/asdf/asdfrc"
+    [ -n "$fpath" ] && fpath=(${ASDF_DATA_DIR}/completions $fpath)
 fi
--- dot_config/exact_zsh/completions.zsh.d/asdf.completion.zsh
+++
@@ -1,3 +0,0 @@
-if [ -f "$ASDF_DATA_DIR/asdf.sh" ]; then
-    fpath=(${ASDF_DATA_DIR}/completions $fpath)
-fi
