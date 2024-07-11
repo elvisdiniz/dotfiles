f59cec42cca31a7ee6d65ffaa95efbc4671c5a75
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-11 01:34:58 +0000

    check if starship is instelled via linuxbrew (homebrew) on linux

--- dot_local/scripts/executable_starship_install.sh
+++ dot_local/scripts/executable_starship_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # check if starship is available on /usr/bin. If not, install on $BIN_HOME
-if [ -x "/usr/bin/starship" ] || [ -x "/usr/local/bin/starship" ]; then
+if [ -x "/usr/bin/starship" ] || [ -x "/usr/local/bin/starship" ] || [ -x "/home/linuxbrew/.linuxbrew/bin/starship" ]; then
     [ -x "$BIN_HOME/starship" ] && rm "$BIN_HOME/starship"
 else
     [ -x "$BIN_HOME/starship" ] || curl -sS https://starship.rs/install.sh | sh -s -- -b $BIN_HOME -y
