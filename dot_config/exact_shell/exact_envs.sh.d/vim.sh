7265d6e74a46daea83102995a2b7952852e1f5f9
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-11 07:37:48 +0000

    fiz viminit bug

--- dot_config/exact_shell/exact_envs.sh.d/vim.sh
+++ dot_config/exact_shell/exact_envs.sh.d/vim.sh
@@ -1,4 +1,11 @@
 if [ -x "$(command -v vim)" ]; then
     [ "$(vim --clean -es +'exec "!echo" has("patch-9.1.0327")' +q)" -eq 0 ] && \
-        export VIMINIT="set nocp | source ${XDG_CONFIG_HOME:-$HOME/.config}/vim/vimrc"
+        export VIMINIT="
+            if has('nvim')
+                so ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/init.lua
+            else
+                set nocp
+                so ${XDG_CONFIG_HOME:-$HOME/.config}/vim/vimrc
+            endif
+        "
 fi
