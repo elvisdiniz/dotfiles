8490bd234e24493f6f693ac4312cd633aeb56845
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-13 18:06:45 +0000

    shell: run .config/shell/init.sh.d scripts on shell load

--- dot_config/exact_shell/init.sh
+++ dot_config/exact_shell/init.sh
@@ -2,4 +2,7 @@
 export SHELL_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}/shell"
 [ -f "${SHELL_CONFIG_HOME}/paths.sh" ] && source "${SHELL_CONFIG_HOME}/paths.sh"
 [ -f "${SHELL_CONFIG_HOME}/envs.sh" ] && source "${SHELL_CONFIG_HOME}/envs.sh"
+for file in ${SHELL_CONFIG_HOME}/init.sh.d/*.sh; do
+    source "$file"
+done
 [ -f "${SHELL_CONFIG_HOME}/aliases.sh" ] && source "${SHELL_CONFIG_HOME}/aliases.sh"
