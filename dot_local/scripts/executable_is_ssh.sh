b4bef9716f61fd42f8dd65c73ac81b86e991cb71
Elvis Diniz <elvisdiniz@outlook.com>
2024-07-13 16:00:28 +0000

    is_ssh.sh: make script compatible com sh rewrite to be compliant with posix

--- dot_local/scripts/executable_is_ssh.sh
+++ dot_local/scripts/executable_is_ssh.sh
@@ -1,9 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-p=${1:-$PPID}
+# set pid as $PPID if first argument is not specified
+p="${1:-$PPID}"
 
-[ "$p" -eq 0 ] && exit 1
-read pid ppid name <<< "$(ps ax -o pid= -o ppid= -o comm= | grep -E -i "^\s*$p\s")"
-[[ "$name" =~ sshd ]] && exit 0
-[[ "$ppid" -le 1 ]] && exit 1
-is_ssh.sh $ppid
+# Use a POSIX compliant way to read output of ps and grep
+# Note: The following may not be the most efficient way in sh, but should work for basic cases
+output="$(ps ax -o pid= -o ppid= -o comm= | grep -E -i "^\s*$p\s")"
+
+# Read variables from the output using POSIX compliant methods
+pid=$(echo "$output" | awk '{print $1}')
+ppid=$(echo "$output" | awk '{print $2}')
+name=$(echo "$output" | awk '{print $3}')
+
+# Check if $name contains "sshd" and exit if true
+if echo "$name" | grep -q "sshd"; then
+    exit 0
+fi
+
+# Check if $ppid is less than or equal to 1 and exit if true
+if [ "$ppid" -le 1 ]; then
+    exit 1
+fi
+
+# Call the script is_ssh.sh with $ppid as argument
+is_ssh.sh "$ppid"
