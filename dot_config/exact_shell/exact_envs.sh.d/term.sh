e9c8712aede63b29b0b17eb82afb4a51faf262ac
Elvis Diniz <elvisdiniz@outlook.com>
2025-02-12 19:37:08 +0000

    term: fix infocmp exec error

--- dot_config/exact_shell/exact_envs.sh.d/term.sh
+++ dot_config/exact_shell/exact_envs.sh.d/term.sh
@@ -1,8 +1,10 @@
 # fallback to xterm-256color or xterm if $TERM is no available
-if ! infocmp $TERM 2> /dev/null > /dev/null; then
-    if infocmp xterm-256color 2> /dev/null > /dev/null; then
-        export TERM=xterm-256color
-    else
-        export TERM=xterm
+if test -x "$(command -v infocmp)"; then
+    if ! infocmp $TERM 2> /dev/null > /dev/null; then
+        if infocmp xterm-256color 2> /dev/null > /dev/null; then
+            export TERM=xterm-256color
+        else
+            export TERM=xterm
+        fi
     fi
 fi
