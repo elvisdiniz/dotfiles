ebcaee3097490947ca5d90872bec12d00e83abff
Elvis Diniz <elvisdiniz@outlook.com>
2025-02-14 09:33:22 +0000

    fish: use abbreviations instead of aliases

--- dot_config/fish/exact_conf.d/15-aliases.fish
+++ dot_config/fish/exact_conf.d/15-aliases.fish
@@ -1,48 +1,50 @@
-# Check if eza exists and set aliases
-if command -v eza > /dev/null
-    alias ls="eza"
-    alias ll="eza -lg"
-    alias la="eza -a"
-    alias lla="eza -lga"
+# Check if eza exists and set abbreviations
+if test -x $(command -v eza)
+    abbr ls eza
+    abbr ll eza -lg
+    abbr la eza -a
+    abbr lla eza -lga
 else
-    alias ll="ls -lh"
-    alias la="ls -a"
-    alias lla="ls -lha"
+    abbr ll ls -lh
+    abbr la ls -a
+    abbr lla ls -lha
 end
 
 # Create a temporary directory and cd into it if mktemp exists
-if command -v mktemp > /dev/null
-    alias cdtemp="cd (mktemp -d)"
+if test -x $(command -v mktemp)
+    abbr cdtemp cd $(mktemp -d)
 end
 
 # Set alternative commands if the preferred ones are missing
-if not command -v vim > /dev/null; and command -v vi > /dev/null
-    alias vim="vi"
+if not test -x $(command -v vim)
+    and test -x $(command -v vi)
+    abbr vim vi
 end
 
-if not command -v dig > /dev/null; and command -v kdig > /dev/null
-    alias dig="kdig"
+if not test -x $(command -v dig)
+    and test -x $(command -v kdig)
+    abbr dig kdig
 end
 
-if not command -v open > /dev/null; and command -v xdg-open > /dev/null
-    alias open="xdg-open"
+if not test -x "$(command -v open)"
+    and test -x "$(command -v xdg-open)"
+    abbr open xdg-open
 end
 
 # WSL-specific configurations
-if command -v notepad++.exe > /dev/null
-    alias npp="notepad++.exe"
+if test -x "$(command -v notepad++.exe)"
+    abbr npp notepad++.exe
 end
 
-if command -v ssh.exe > /dev/null
-    alias ssh="ssh.exe"
+if test -x "$(command -v ssh.exe)"
+    abbr ssh ssh.exe
 end
 
-if command -v git.exe > /dev/null
-    alias git="git.exe"
+if test -x "$(command -v git.exe)"
+    abbr git git.exe
 end
 
-# Create lazydocker alias if available
-if command -v lazydocker > /dev/null
-    alias lzd="lazydocker"
+# Create lazydocker abbreviation if available
+if test -x "$(command -v lazydocker)"
+    abbr lzd lazydocker
 end
-
