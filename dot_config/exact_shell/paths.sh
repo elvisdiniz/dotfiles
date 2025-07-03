4b468a465345c62474b222e88673621df29aa1e2
Elvis Diniz <elvisdiniz@outlook.com>
2025-07-03 00:52:08 +0000

    refactor: use double bracklets instead of single

--- dot_config/exact_shell/paths.sh
+++ dot_config/exact_shell/paths.sh
@@ -1,19 +1,17 @@
 # set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
-fi
+[[ -d "$HOME/bin" ]] && export PATH="$HOME/bin:$PATH"
 
 # set PATH so it includes user's private .local/bin if it exists
 export BIN_HOME="${HOME}/.local/bin"
-[ -d "$BIN_HOME" ] || mkdir -p "$BIN_HOME"
-PATH="$BIN_HOME:$PATH"
+[[ -d "$BIN_HOME" ]] || mkdir -p "$BIN_HOME"
+export PATH="$BIN_HOME:$PATH"
 
 # set PATH so it includes user's private .local/bin if it exists
 export SCRIPTS_HOME="${HOME}/.local/scripts"
-[ -d "$SCRIPTS_HOME" ] || mkdir -p "$SCRIPTS_HOME"
-PATH="$SCRIPTS_HOME:$PATH"
+[[ -d "$SCRIPTS_HOME" ]] || mkdir -p "$SCRIPTS_HOME"
+export PATH="$SCRIPTS_HOME:$PATH"
 
 export NPM_BIN="${HOME}/.local/share/npm/bin"
-[ -d "$NPM_BIN" ] && PATH="$NPM_BIN:$PATH"
+[[ -d "$NPM_BIN" ]] && export PATH="$NPM_BIN:$PATH"
 
-[[ -x "$(command -v asdf)" ]] && PATH="${ASDF_DATA_DIR:-$HOME/.local/share/asdf}/shims:$PATH"
+[[ -x "$(command -v asdf)" ]] && export PATH="${ASDF_DATA_DIR:-$HOME/.local/share/asdf}/shims:$PATH"
