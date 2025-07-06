1b33ae57b9de74d20b4d46b31b7b9bca036f3a14
Elvis Diniz <elvisdiniz@outlook.com>
2025-07-06 18:46:46 +0000

    scripts: delete install_asdf.sh script

--- dot_local/exact_scripts/executable_install_asdf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# clone asdf repository on $ASDF_DATA_DIR, default is $XDG_DATA_HOME/asdf
-# or update if is already there
-if [ -f "${ASDF_DATA_DIR}/asdf.sh" ]; then
-    asdf update
-else
-    git clone https://github.com/asdf-vm/asdf.git $ASDF_DATA_DIR
-fi
