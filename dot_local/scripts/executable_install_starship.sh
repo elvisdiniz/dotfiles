195bc2433d6d441e290dd7d900abba4be52a4ba6
Elvis Diniz <elvisdiniz@outlook.com>
2025-07-06 13:44:29 -0300

    scripts: update starship installation script for LXC container compatibility

--- dot_local/scripts/executable_install_starship.sh
+++ dot_local/scripts/executable_install_starship.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
+# if is inside a lxc container, download custom starship version
+if [ "$(cat /var/run/systemd/container 2> /dev/null)" = "lxc" ]; then
+    cd $BIN_HOME
+    wget -q https://github.com/elvisdiniz/starship/releases/download/v1.23.0-custom/starship-x86_64-unknown-linux-gnu.tar.gz
+    tar xvf starship-x86_64-unknown-linux-gnu.tar.gz
+    rm starship-x86_64-unknown-linux-gnu.tar.gz
 # check if starship is available on /usr/bin. If not, install on $BIN_HOME
-if [ -x "/usr/bin/starship" ] || [ -x "/usr/local/bin/starship" ] || [ -x "/home/linuxbrew/.linuxbrew/bin/starship" ]; then
+elif [ -x "/usr/bin/starship" ] || [ -x "/usr/local/bin/starship" ] || [ -x "/home/linuxbrew/.linuxbrew/bin/starship" ]; then
     [ -x "$BIN_HOME/starship" ] && rm "$BIN_HOME/starship"
 else
     [ -x "$BIN_HOME/starship" ] || curl -sS https://starship.rs/install.sh | sh -s -- -b $BIN_HOME -y
