3e13c39544951f1d1260657fc73cb151a858f426
Elvis Diniz <elvisdiniz@outlook.com>
2025-07-06 13:51:20 -0300

    fix: standardize bracket usage in run_after_load_zsh.sh

--- .chezmoiscripts/run_after_load_zsh.sh
+++ .chezmoiscripts/run_after_load_zsh.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-[[ -x "$(command -v zsh)" ]] && zsh -c "source $HOME/.zshenv; source $HOME/.config/zsh/.zshrc; exit"
+[ -x "$(command -v zsh)" ] && zsh -c "source $HOME/.zshenv; source $HOME/.config/zsh/.zshrc; exit"
