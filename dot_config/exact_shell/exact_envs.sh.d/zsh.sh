3c19efa70b5f7a141822be8bde2ce654af9f98f9
Elvis Diniz <elvisdiniz@outlook.com>
2025-07-07 15:50:28 +0000

    zsh: create ZSH_CACHE_DIR, if not exists

--- dot_config/exact_shell/exact_envs.sh.d/zsh.sh
+++ dot_config/exact_shell/exact_envs.sh.d/zsh.sh
@@ -1,5 +1,6 @@
 if [ -x "$(command -v zsh)" ]; then
     ZSH_STATE_HOME="${XDG_STATE_HOME:-${HOME}/.local/state}/zsh"
-    [ -d "${XDG_CACHE_HOME:-${HOME}/.cache}/zsh" ] || mkdir -p "${XDG_CACHE_HOME:-${HOME}/.cache}/zsh"
+    ZSH_CACHE_DIR="${XDG_CACHE_HOME:-${HOME}/.cache}/zsh"
     [ -d "$ZSH_STATE_HOME" ] || mkdir -p "$ZSH_STATE_HOME"
+    [ -d "$ZSH_CACHE_DIR" ] || mkdir -p "$ZSH_CACHE_DIR"
 fi
