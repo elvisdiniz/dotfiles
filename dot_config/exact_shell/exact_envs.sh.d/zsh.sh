c598cdcd96e7b9d840c79d8dc259036b2d32b8bc
Elvis Diniz <elvisdiniz@outlook.com>
2025-07-07 16:02:37 +0000

    zsh: fix zsh cache dir creation

--- dot_config/exact_shell/exact_envs.sh.d/zsh.sh
+++ dot_config/exact_shell/exact_envs.sh.d/zsh.sh
@@ -2,5 +2,5 @@ if [ -x "$(command -v zsh)" ]; then
     ZSH_STATE_HOME="${XDG_STATE_HOME:-${HOME}/.local/state}/zsh"
     ZSH_CACHE_DIR="${XDG_CACHE_HOME:-${HOME}/.cache}/zsh"
     [ -d "$ZSH_STATE_HOME" ] || mkdir -p "$ZSH_STATE_HOME"
-    [ -d "$ZSH_CACHE_DIR" ] || mkdir -p "$ZSH_CACHE_DIR"
+    [ -d "$ZSH_CACHE_DIR/completions" ] || mkdir -p "$ZSH_CACHE_DIR/completions"
 fi
